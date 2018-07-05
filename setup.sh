1b15f446223f41da52b3b1e13229953d86baac1c
Andrey V. Romanchev <andrey.romanchev@gmail.com>
2018-07-06 00:15:46 +0300

    add zsh plugins

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 # TODO: oh-my-zsh installing
 # TODO: move to ansible
 sudo apt-get install fonts-powerline
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+vim .zshrc
