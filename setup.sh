ecd66a15e33cc14541cc4263bbe23aa94252ba95
Andrey Romanchev <aromanchev@wallarm.com>
2018-12-26 15:14:17 +0300

    add packages and repos

--- setup.sh
+++ setup.sh
@@ -1,6 +1,29 @@
+apt install software-properties-common
+apt-add-repository --yes ppa:ansible/ansible
+apt install ansible
+
+sudo ansible-playbook ansible-setup.yml
+
 # TODO: oh-my-zsh installing
 # TODO: move to ansible
-sudo apt-get install fonts-powerline
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 vim .zshrc
+# ZSH_THEME="agnoster"
+#
+# plugins=(
+#  git
+#  zsh-autosuggestions
+#  zsh-syntax-highlighting
+#  colored-man-pages
+#  command-not-found
+#  docker
+#  docker-compose
+#  docker-machine
+#  helm
+#  kubectl
+#  kube-ps1
+#  minikube
+#  rsync
+#  tmux
+#)
